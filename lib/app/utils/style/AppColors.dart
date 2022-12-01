43a5318f2d92bfe8253ccc9a60deba157ec81f14
FernandJerico <fernand.jerico.ti.20@cic.ac.id>
2022-12-01 09:43:20 +0700

    commit create widget

---
+++ lib/app/utils/style/AppColors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primaryBg = Color.fromRGBO(239, 242, 247, 1);
+  static const cardBg = Color(0xffe3e3ee);
+  static const primaryText = Color(0xFF000000);
+  static const primaryButton = Color.fromRGBO(98, 144, 142, 1);
+}
---
+++ lib/app/utils/widget/BottomNavbar.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:inventaris_app/app/modules/home/views/home_view.dart';
+
+import '../../modules/history/views/history_view.dart';
+import '../../modules/items/views/items_view.dart';
+import '../../modules/profile/views/profile_view.dart';
+
+class BottomNavbar extends StatelessWidget {
+  const BottomNavbar({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 16),
+        child: GNav(
+            backgroundColor: Colors.white,
+            color: Colors.black,
+            activeColor: Color.fromARGB(255, 0, 0, 0),
+            tabBackgroundColor: Color.fromARGB(255, 226, 222, 222),
+            gap: 6,
+            padding: EdgeInsets.all(16),
+            tabs: [
+              GButton(
+                icon: Icons.home,
+                text: "Home",
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return HomeView();
+                  }));
+                },
+              ),
+              GButton(
+                icon: Icons.view_in_ar,
+                text: "Items",
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return ItemsView();
+                  }));
+                },
+              ),
+              GButton(
+                icon: Icons.history,
+                text: "History",
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return HistoryView();
+                  }));
+                },
+              ),
+              GButton(
+                icon: Icons.person,
+                text: "Profile",
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return ProfileView();
+                  }));
+                },
+              )
+            ]),
+      ),
+    );
+  }
+}
